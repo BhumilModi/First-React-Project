3aa6970e18a17044a84423270c9819b1659d3da9
Bhumil Modi <bhumilmodi2002@gmail.com>
2021-10-23 14:34:48 +0530

    First Checkpoint

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const Pet = (props) => {
+    return React.createElement("div" , {} , [
+        React.createElement("h2" , {} , props.name),
+        React.createElement("h3" , {} , props.animal),
+        React.createElement("h3" , {} , props.breed),
+    ])
+}
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        [React.createElement("hi" , {} , "Adopt-Me!!"),
+        React.createElement(Pet , {name:"Rocky" , animal:"dog" , breed:'husky'}),
+        React.createElement(Pet , {name:"Billu" , animal:"Bird" , breed:'Parrot'}),
+        React.createElement(Pet , {name:"Mau" , animal:"Cat" , breed:'Bengal Cat'}),]
+    );
+};
+
+ReactDOM.render(React.createElement(App) , document.getElementById('root') );
